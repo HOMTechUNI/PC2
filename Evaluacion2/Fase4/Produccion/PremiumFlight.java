65024073c230d42c3559c739ee397de23f81880f
Bobter <renzo.oyola.r@uni.pe>
2022-06-02 13:28:02 -0500

    Update PremiumFlight

--- Evaluacion2/Fase4/Produccion/PremiumFlight.java
+++ Evaluacion2/Fase4/Produccion/PremiumFlight.java
@@ -1,3 +1,12 @@
 public class PremiumFlight extends Flight {
-  // Diseño inicial de la clase  PremiumFlight. Pregunta 5
+    public PremiumFlight(String id){super(id);}
+    @Override
+    public boolean addPassenger(Passenger passenger) {
+        return false;
+    }
+
+    @Override
+    public boolean removePassenger(Passenger passenger) {
+        return false;
+    }
 }
